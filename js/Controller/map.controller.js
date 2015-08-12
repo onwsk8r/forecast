40ae4be2a9bf734cb9d7442cfa5ff6b4705653d2
Brian Hazeltine <brian@wasthat.me>
2015-08-11 21:10:04 -0500

    Create layer viewer

--- js/Controller/map.controller.js
+++ js/Controller/map.controller.js
@@ -8,9 +8,29 @@
     mapController.$inject = ['$http', '$scope'];
 
     function mapController($http, $scope) {
+        $scope.layers = [];
+
+        $http.get('http://172.16.24.10:8080/geoserver/ndfd/ows?service=wms&version=1.3.0&request=GetCapabilities').
+        then(function (response) {
+            var parser = new ol.format.WMSCapabilities();
+            var result = parser.read(response.data);
+            $scope.pushLayers(result.Capability.Layer.Layer);
+            console.log($scope.layers);
+        }, function (response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+
+        $scope.pushLayers = function(data) {
+            // TODO make namespaces a thing
+            angular.forEach(data, function (value) {
+                this.push(value);
+            }, $scope.layers);
+        }
+
         var srtm = new ol.layer.Tile({
             source: new ol.source.TileWMS({
-               // url: 'https://d1zy9frnzrb6ns.cloudfront.net/geoserver/wms',
+                // url: 'https://d1zy9frnzrb6ns.cloudfront.net/geoserver/wms',
                 url: 'http://172.16.24.10:8080/geoserver/wms',
                 params: {
                     'LAYERS': 'srtm:bw-comp',
@@ -23,14 +43,15 @@
         });
 
         var warning = new ol.layer.Tile({
-            opacity: 0.8,
+            opacity: 0.6,
             source: new ol.source.TileWMS({
-               // url: 'https://d1zy9frnzrb6ns.cloudfront.net/geoserver/wms',
+                // url: 'https://d1zy9frnzrb6ns.cloudfront.net/geoserver/wms',
                 url: 'http://172.16.24.10:8080/geoserver/wms',
                 params: {
                     'LAYERS': 'ndfd:Temperature_surface',
-                    'TILED': true
-                    //'time': '2015-08-10T12:00:00.000Z'
+                    'TILED': true,
+                    'format': "image/jpeg"
+                        //'time': '2015-08-10T12:00:00.000Z'
                 },
                 serverType: 'geoserver'
             })
@@ -43,6 +64,5 @@
                 zoom: 5
             })
         });
-console.log(warningMap);
     }
 })();
